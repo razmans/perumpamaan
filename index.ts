a9b912c39182a5791f55b076a602eb01095c5641
razmans <razmans@gmail.com>
2024-10-24 01:26:26 +0800

    Add Perumpamaan Module and related functions

--- index.ts
+++ index.ts
@@ -12,12 +12,29 @@ export const getRandomPerumpamaan = (): Quote => {
   return dictionary[randomIndex];
 };
 
+/**
+ * @module getPerumpamaanWithKeyword
+ * @description Mengambil perumpamaan yang mengandungi kata kunci
+ * @param keyword
+ * @returns {Quote[]} Perumpamaan yang mengandungi kata kunci
+ * @example
+ * ```const perumpamaan = getPerumpamaanWithKeyword('pagar')```;
+ */
+
 export const getPerumpamaanWithKeyword = (keyword: string): Quote[] => {
   return dictionary.filter(
     (quote) => quote.quote.includes(keyword) || quote.meaning.includes(keyword)
   );
 };
 
+/**
+ * @module getRandomPerumpamaanExcludingKeyword
+ * @description Mengambil perumpamaan secara rawak dari senarai perumpamaan, kecuali perumpamaan yang dinyatakan
+ * @param excludedQuotes
+ * @returns {Quote}
+ * @example
+ * ```const perumpamaan = getRandomPerumpamaanExcludingKeyword('mengikut')```;
+ */
 export const getRandomPerumpamaanExcludingKeyword = (
   excludedQuotes: string
 ): Quote => {
